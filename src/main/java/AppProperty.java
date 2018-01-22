41cda9b68c4f0fb6629da46b16ab946723fe10f9
alwxander.diachenko <mail.positiv@gmail.com>
2018-01-22 16:56:53 +0200

    данные считывабтся с проперти файла

---
+++ src/main/java/AppProperty.java
@@ -0,0 +1,19 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author Alexander Diachenko.
+ */
+public class AppProperty {
+
+    public static String getProperty(String property) throws IOException {
+        Properties mainProperties = new Properties();
+        FileInputStream file;
+        String path = "./main.properties";
+        file = new FileInputStream(path);
+        mainProperties.load(file);
+        file.close();
+        return mainProperties.getProperty(property);
+    }
+}

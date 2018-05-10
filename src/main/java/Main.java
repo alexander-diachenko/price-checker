618a93ba02ffa367ff0870b8254c8e9671b2bb88
alwxander.diachenko <mail.positiv@gmail.com>
2018-05-10 12:31:33 +0300

    теперь классы сами определяют они ли должны обрабатывать запрос

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,10 @@ import magazine.Makeup;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import property.AppProperty;
+import url.AppProperty;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
@@ -25,26 +26,30 @@ public class Main {
     }
 
     private final static Logger logger = Logger.getLogger(Main.class);
+    private static WebDriver driver;
 
     public static void main(String[] args) {
         Properties properties = AppProperty.getProperty();
-        WebDriver driver = getDriver(BrowserVersion.CHROME, true);
-        Magazine makeup = new Makeup(driver, properties);
+        List<Magazine> magazines = getMagazines(properties);
         Excel excel = new ExcelImpl();
         try {
-            List<List<Object>>  table = excel.read(properties.getProperty("file.path"));
+            List<List<Object>> table = excel.read(properties.getProperty("file.path"));
 
             final String priceColumn = properties.getProperty("price.column");
             final String linkColumn = properties.getProperty("link.column");
             for (int i = 0, tableSize = table.size(); i < tableSize; i++) {
+                driver.get("https://www.google.com.ua/");
                 List<Object> row = table.get(i);
                 final String url = String.valueOf(row.get(Integer.parseInt(linkColumn) - 1));
-                if (!url.isEmpty()) {
-                    driver.get("https://www.google.com.ua/");
-                    final int column = Integer.parseInt(priceColumn) - 1;
-                    final String price = makeup.getPrice(url);
-                    System.out.println((i + 1) + ") " + url + " -> " + price + " грн");
-                    insert(row, column, price);
+                for (Magazine magazine : magazines) {
+                    if (magazine.isThisWebsite(url)) {
+                        if (!url.isEmpty()) {
+                            int column = Integer.parseInt(priceColumn) - 1;
+                            String price = magazine.getPrice(url);
+                            System.out.println((i + 1) + ") " + url + " -> " + price + " грн");
+                            insert(row, column, price);
+                        }
+                    }
                 }
             }
 
@@ -54,6 +59,14 @@ public class Main {
         } finally {
             closeConnection(driver);
         }
+    }
+
+    private static List<Magazine> getMagazines(Properties properties) {
+        List<Magazine> magazines = new ArrayList<>();
+        driver = getDriver(BrowserVersion.CHROME, true);
+        Magazine makeup = new Makeup(driver, properties);
+        magazines.add(makeup);
+        return magazines;
     }
 
     private static void insert(List<Object> row, int column, String price) {
@@ -69,7 +82,7 @@ public class Main {
     }
 
     private static WebDriver getDriver(BrowserVersion browserVersion, boolean javascript) {
-        return new HtmlUnitDriver(browserVersion,javascript) {
+        return new HtmlUnitDriver(browserVersion, javascript) {
             @Override
             protected WebClient newWebClient(BrowserVersion version) {
                 WebClient webClient = super.newWebClient(version);

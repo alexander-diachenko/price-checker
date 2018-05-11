d715196570555bb915d1b06055b683a5301585cf
alwxander.diachenko <mail.positiv@gmail.com>
2018-05-11 15:35:11 +0300

    рефакторинг

--- src/main/java/controller/MainService.java
+++ src/main/java/controller/MainService.java
@@ -26,15 +26,17 @@ public class MainService extends Service<Void> {
 
     private final static Logger logger = Logger.getLogger(MainService.class);
 
-    private File file;
-    private final Spinner<Integer> urlColumn;
-    private final Spinner<Integer> insertColumn;
+    private String filePath;
+    private Integer urlColumn;
+    private Integer insertColumn;
+    private String saveDirectoryPath;
     private ProgressIndicator progressIndicator;
 
-    public MainService(File file, Spinner<Integer> urlColumn, Spinner<Integer> insertColumn, ProgressIndicator progressIndicator) {
-        this.file = file;
+    public MainService(String filePath, Integer urlColumn, Integer insertColumn, String saveDirectoryPath, ProgressIndicator progressIndicator) {
+        this.filePath = filePath;
         this.urlColumn = urlColumn;
         this.insertColumn = insertColumn;
+        this.saveDirectoryPath = saveDirectoryPath;
         this.progressIndicator = progressIndicator;
     }
 
@@ -50,28 +52,27 @@ public class MainService extends Service<Void> {
                     driver = getDriver(BrowserVersion.CHROME, true);
                     Properties properties = AppProperty.getProperty();
                     List<Magazine> magazines = getMagazines(driver, properties);
-                    List<List<Object>> table = excel.read(file.getPath());
+                    List<List<Object>> table = excel.read(filePath);
                     for (int index = 0; index < table.size(); index++) {
                         List<Object> row = table.get(index);
-                        String url = String.valueOf(row.get(urlColumn.getValue() - 1));
+                        String url = String.valueOf(row.get(urlColumn - 1));
                         for (Magazine magazine : magazines) {
                             if (!url.isEmpty() && magazine.isThisWebsite(url)) {
                                 String price = magazine.getPrice(url);
                                 System.out.println((index + 1) + ") " + url + " -> " + price);
-                                insert(row, insertColumn.getValue() - 1, price);
+                                insert(row, insertColumn - 1, price);
                             }
                         }
                         int finalIndex = index;
-                        Platform.runLater(()->progressIndicator.setProgress(0.99 / table.size() * (finalIndex + 1)));
+                        Platform.runLater(() -> progressIndicator.setProgress(0.99 / table.size() * (finalIndex + 1)));
                     }
-                    excel.write(table, properties.getProperty("save.file.path"));
+                    excel.write(table, saveDirectoryPath + "\\prices.xlsx");
                     final long end = new Date().getTime();
                     System.out.println(end - start);
                 } catch (Exception exception) {
                     logger.error(exception.getMessage(), exception);
                     throw exception;
-                }
-                finally {
+                } finally {
                     Objects.requireNonNull(driver).close();
                 }
                 return null;

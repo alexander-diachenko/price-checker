3b2d1353b219338a974aa5b4fdd825a7bbc4508f
alwxander.diachenko <mail.positiv@gmail.com>
2018-05-11 15:10:20 +0300

    драйвер создается в потоке таски

--- src/main/java/controller/MainService.java
+++ src/main/java/controller/MainService.java
@@ -4,9 +4,11 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import excel.Excel;
 import excel.ExcelImpl;
+import javafx.application.Platform;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.Spinner;
 import magazine.Magazine;
 import magazine.Makeup;
 import org.apache.log4j.Logger;
@@ -14,9 +16,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import url.AppProperty;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.io.File;
+import java.util.*;
 
 /**
  * @author Alexander Diachenko.
@@ -25,18 +26,16 @@ public class MainService extends Service<Void> {
 
     private final static Logger logger = Logger.getLogger(MainService.class);
 
-    private Properties properties;
-    private WebDriver driver;
-    private Excel excel;
-    private List<Magazine> magazines;
+    private File file;
+    private final Spinner<Integer> urlColumn;
+    private final Spinner<Integer> insertColumn;
     private ProgressIndicator progressIndicator;
 
-    public MainService(ProgressIndicator progressIndicator) {
+    public MainService(File file, Spinner<Integer> urlColumn, Spinner<Integer> insertColumn, ProgressIndicator progressIndicator) {
+        this.file = file;
+        this.urlColumn = urlColumn;
+        this.insertColumn = insertColumn;
         this.progressIndicator = progressIndicator;
-        properties = AppProperty.getProperty();
-        driver = getDriver(BrowserVersion.CHROME, true);
-        excel = new ExcelImpl();
-        magazines = getMagazines(driver, properties);
     }
 
     @Override
@@ -44,28 +43,36 @@ public class MainService extends Service<Void> {
         return new Task<Void>() {
             @Override
             protected Void call() throws Exception {
+                final long start = new Date().getTime();
+                WebDriver driver = null;
+                Excel excel = new ExcelImpl();
                 try {
-                    List<List<Object>> table = excel.read(properties.getProperty("file.path"));
-                    final String priceColumn = properties.getProperty("price.column");
-                    final String linkColumn = properties.getProperty("link.column");
+                    driver = getDriver(BrowserVersion.CHROME, true);
+                    Properties properties = AppProperty.getProperty();
+                    List<Magazine> magazines = getMagazines(driver, properties);
+                    List<List<Object>> table = excel.read(file.getPath());
                     for (int index = 0; index < table.size(); index++) {
                         List<Object> row = table.get(index);
-                        final String url = String.valueOf(row.get(Integer.parseInt(linkColumn) - 1));
+                        String url = String.valueOf(row.get(urlColumn.getValue() - 1));
                         for (Magazine magazine : magazines) {
                             if (!url.isEmpty() && magazine.isThisWebsite(url)) {
-                                int column = Integer.parseInt(priceColumn) - 1;
                                 String price = magazine.getPrice(url);
                                 System.out.println((index + 1) + ") " + url + " -> " + price);
-                                insert(row, column, price);
+                                insert(row, insertColumn.getValue() - 1, price);
                             }
                         }
-                        progressIndicator.setProgress((double) 1 / table.size() * (index + 1));
+                        int finalIndex = index;
+                        Platform.runLater(()->progressIndicator.setProgress(0.99 / table.size() * (finalIndex + 1)));
                     }
                     excel.write(table, properties.getProperty("save.file.path"));
-                } catch (Exception e) {
-                    logger.error(e.getMessage(), e);
-                } finally {
-                    driver.close();
+                    final long end = new Date().getTime();
+                    System.out.println(end - start);
+                } catch (Exception exception) {
+                    logger.error(exception.getMessage(), exception);
+                    throw exception;
+                }
+                finally {
+                    Objects.requireNonNull(driver).close();
                 }
                 return null;
             }

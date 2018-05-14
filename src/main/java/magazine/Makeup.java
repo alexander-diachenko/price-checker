d4c866b840c7ab517bcd3cdc5409fbeee975e001
alwxander.diachenko <mail.positiv@gmail.com>
2018-05-14 10:13:51 +0300

    временно

--- src/main/java/magazine/Makeup.java
+++ src/main/java/magazine/Makeup.java
@@ -1,13 +1,17 @@
 package magazine;
 
 import org.apache.log4j.Logger;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import url.UrlUtils;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Properties;
 
@@ -19,17 +23,16 @@ public class Makeup implements Magazine {
     private final static Logger logger = Logger.getLogger(Makeup.class);
 
     private Properties properties;
-    private WebDriver driver;
+    private String id;
 
-    public Makeup(WebDriver driver, Properties properties) {
-        this.driver = driver;
+    public Makeup(Properties properties) {
         this.properties = properties;
     }
 
     @Override
     public String getPrice(String url) {
         Document document = getDocument(url);
-        if (!isCorrectPage(document)) {
+        if (document == null) {
             return "Страница не найдена";
         }
         return getValue(document);
@@ -52,19 +55,35 @@ public class Makeup implements Magazine {
 
     @Override
     public Document getDocument(String url) throws WebDriverException {
-        driver.get("https://www.google.com.ua/");
-        driver.get(url);
-        final String page = driver.getPageSource();
-        return Jsoup.parse(page);
+        String[] split = url.split("/");
+        id = split[split.length - 1];
+        try {
+            return Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
+                    .get();
+        } catch (HttpStatusException ex) {
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.getMessage(), e);
+        }
+        return null;
     }
 
     private String getValue(Document document) {
         if (!isAvailable(document)) {
             return "Нет в наличии";
-        } else if (isDiscount(document)) {
-            return document.select(properties.getProperty("discount.price.span")).first().text();
-        } else
-            return document.select(properties.getProperty("normal.price.span")).first().text();
+        } else {
+            Elements withAttr = new Elements();
+            for (Element element : document.getAllElements()) {
+                for (Attribute attribute : element.attributes()) {
+                    if (attribute.getValue().equalsIgnoreCase(String.valueOf(id))) {
+                        withAttr.add(element);
+                    }
+                }
+            }
+            return withAttr.get(0).attr("data-price");
+        }
     }
 
     @Override

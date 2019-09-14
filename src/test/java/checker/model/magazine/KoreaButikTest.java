fa98d50ca89db46229970bb45316f8e6586d9762
alexander-diachenko <mail.positiv@gmail.com>
2019-09-14 08:23:14 +0300

    добавил новый магазин korea-butik.com

---
+++ src/test/java/checker/model/magazine/KoreaButikTest.java
@@ -0,0 +1,39 @@
+package checker.model.magazine;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Alexander Diachenko
+ */
+public class KoreaButikTest {
+
+    private KoreaButik koreaButik;
+    private DocumentCreator creator;
+
+    @Before
+    public void setup() {
+        koreaButik = new KoreaButik();
+        creator = new DocumentCreator();
+    }
+
+    @Test
+    public void shouldReturnDiscountPrice() {
+        String price = koreaButik.getValue(creator.createDocumentFromFile("KoreaButik_discount.xml"));
+        assertEquals("337,50", price);
+    }
+
+    @Test
+    public void shouldReturnNormalPrice() {
+        String price = koreaButik.getValue(creator.createDocumentFromFile("KoreaButik_normal.xml"));
+        assertEquals("145", price);
+    }
+
+    @Test
+    public void shouldReturnOutOfStock() {
+        String price = koreaButik.getValue(creator.createDocumentFromFile("KoreaButik_outofstock.xml"));
+        assertEquals("Нет в наличии", price);
+    }
+}
---
+++ src/test/java/integration/KoreaButikTest.java
@@ -0,0 +1,31 @@
+package integration;
+
+import checker.model.magazine.Korea;
+import checker.model.magazine.Magazine;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * @author Alexander Diachenko.
+ */
+public class KoreaButikTest {
+
+    private Magazine korea = new Korea();
+
+    @Test
+    public void shouldReturnPageNotFound() {
+        String price = korea.getPrice("https://korea-butik.com/qwe");
+        assertEquals("Страница не найдена", price);
+    }
+
+    @Test
+    public void shouldReturnNotEmptyDocument() throws IOException {
+        Document document = korea.getDocument("https://korea-butik.com");
+        assertFalse(document.data().isEmpty());
+    }
+}

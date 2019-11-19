93961c6187d6d2cdf4ae0585fde26a031e227807
oleksandr-diachenko <mail.positiv@gmail.com>
2019-11-19 22:32:17 +0200

    добавил тесты для ексель утил

--- src/main/java/checker/model/excel/ExcelImpl.java
+++ src/main/java/checker/model/excel/ExcelImpl.java
@@ -1,12 +1,14 @@
 package checker.model.excel;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.SheetUtil;
 import org.apache.poi.ss.util.WorkbookUtil;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -22,53 +24,52 @@ public class ExcelImpl implements Excel {
      * @param path Path to excel file.
      * @return List representation of excel file.
      * @throws IOException            Throws IOException if file read failed.
-     * @throws InvalidFormatException Throws InvalidFormatException if it is not excel file(.xls or .xlsx).
      */
     @Override
-    public List<List<Object>> read(final String path) throws IOException, InvalidFormatException {
-        final List<List<Object>> table = new ArrayList<>();
-        final Workbook workbook = getWorkbook(path);
+    public List<List<Object>> read(String path) throws IOException {
+        List<List<Object>> table = new ArrayList<>();
+        Workbook workbook = getWorkbook(path);
         for (int sheetIndex = 0; sheetIndex < workbook.getNumberOfSheets(); sheetIndex++) {
-            final Sheet sheet = workbook.getSheetAt(sheetIndex);
+            Sheet sheet = workbook.getSheetAt(sheetIndex);
             table.addAll(getTableFromSheet(sheet));
         }
         return table;
     }
 
-    private Workbook getWorkbook(final String path) throws IOException, InvalidFormatException {
-        try (final FileInputStream is = new FileInputStream(new File(path))) {
+    private Workbook getWorkbook(String path) throws IOException {
+        try (FileInputStream is = new FileInputStream(new File(path))) {
             return WorkbookFactory.create(is);
         }
     }
 
-    private List<List<Object>> getTableFromSheet(final Sheet sheet) {
-        final List<List<Object>> table = new ArrayList<>();
-        final Iterator rows = sheet.rowIterator();
+    private List<List<Object>> getTableFromSheet(Sheet sheet) {
+        List<List<Object>> table = new ArrayList<>();
+        Iterator rows = sheet.rowIterator();
         while (rows.hasNext()) {
             table.add(getRaw(rows));
         }
         return table;
     }
 
-    private List<Object> getRaw(final Iterator rows) {
-        final List<Object> raw = new ArrayList<>();
-        final Row row = (Row) rows.next();
+    private List<Object> getRaw(Iterator rows) {
+        List<Object> raw = new ArrayList<>();
+        Row row = (Row) rows.next();
         int index = 0;
-        final short lastCellNum = row.getLastCellNum();
+        short lastCellNum = row.getLastCellNum();
         while (index < lastCellNum) {
             raw.add(getCell(row, index++));
         }
         return raw;
     }
 
-    private String getCell(final Row row, final int index) {
-        final Cell cell = row.getCell(index, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+    private String getCell(Row row, int index) {
+        Cell cell = row.getCell(index, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
         return getFormattedCell(cell);
     }
 
-    private String getFormattedCell(final Cell cell) {
-        final DataFormatter df = new DataFormatter();
-        final String result = df.formatCellValue(cell);
+    private String getFormattedCell(Cell cell) {
+        DataFormatter df = new DataFormatter();
+        String result = df.formatCellValue(cell);
         return !result.equals("#NULL!") ? result : "";
     }
 
@@ -80,15 +81,15 @@ public class ExcelImpl implements Excel {
      * @throws IOException Throws IOException if file write failed.
      */
     @Override
-    public void write(final List<List<Object>> table, final String path) throws IOException {
-        final Workbook workbook = new XSSFWorkbook();
-        final Sheet sheet = workbook.createSheet(WorkbookUtil.createSafeSheetName("New sheet"));
+    public void write(List<List<Object>> table, String path) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet(WorkbookUtil.createSafeSheetName("New sheet"));
         for (int rawIndex = 0; rawIndex < table.size(); rawIndex++) {
-            final List<Object> raw = table.get(rawIndex);
-            final Row row = sheet.createRow(rawIndex);
+            List<Object> raw = table.get(rawIndex);
+            Row row = sheet.createRow(rawIndex);
             for (int colIndex = 0; colIndex < raw.size(); colIndex++) {
-                final Object obj = raw.get(colIndex);
-                final Cell cell = row.createCell(colIndex);
+                Object obj = raw.get(colIndex);
+                Cell cell = row.createCell(colIndex);
                 cell.setCellValue(String.valueOf(obj));
             }
         }
@@ -96,8 +97,8 @@ public class ExcelImpl implements Excel {
         write(workbook, path);
     }
 
-    private void write(final Workbook workbook, final String path) throws IOException {
-        try (final FileOutputStream outputStream = new FileOutputStream(new File(path))) {
+    private void write(Workbook workbook, String path) throws IOException {
+        try (FileOutputStream outputStream = new FileOutputStream(new File(path))) {
             workbook.write(outputStream);
         }
     }
@@ -109,11 +110,11 @@ public class ExcelImpl implements Excel {
      * @return int column count.
      */
     @Override
-    public int getColumnCount(final Sheet sheet) {
+    public int getColumnCount(Sheet sheet) {
         int columnCount = 0;
         for (int rowNumber = 0; rowNumber <= sheet.getLastRowNum(); rowNumber++) {
-            final Row row = sheet.getRow(rowNumber);
-            final short lastCellNum = row.getLastCellNum();
+            Row row = sheet.getRow(rowNumber);
+            short lastCellNum = row.getLastCellNum();
             columnCount = Math.max(columnCount, lastCellNum);
         }
         return columnCount;
@@ -125,7 +126,7 @@ public class ExcelImpl implements Excel {
      * @param sheet of table
      */
     @Override
-    public void autoResizeSheet(final Sheet sheet) {
+    public void autoResizeSheet(Sheet sheet) {
         for (int columnIndex = 0; columnIndex < getColumnCount(sheet); columnIndex++) {
             if (isEmpty(sheet, columnIndex)) {
                 hideColumn(sheet, columnIndex);
@@ -134,12 +135,12 @@ public class ExcelImpl implements Excel {
         }
     }
 
-    private boolean isEmpty(final Sheet sheet, final int index) {
-        final double columnWidth = SheetUtil.getColumnWidth(sheet, index, true);
+    private boolean isEmpty(Sheet sheet, int index) {
+        double columnWidth = SheetUtil.getColumnWidth(sheet, index, true);
         return columnWidth < 2;
     }
 
-    private void hideColumn(final Sheet sheet, final int index) {
+    private void hideColumn(Sheet sheet, int index) {
         sheet.setColumnHidden(index, true);
     }
 }

3f78ae6cdd2d3f83d386cf19e99eed07188424a0
oleksandr-diachenko <mail.positiv@gmail.com>
2020-12-30 13:03:20 +0200

    Used price checker core dependency

--- src/main/java/checker/service/MainService.java
+++ src/main/java/checker/service/MainService.java
@@ -1,25 +1,26 @@
 package checker.service;
 
-import checker.model.excel.Excel;
-import checker.model.excel.ExcelImpl;
-import checker.model.magazine.*;
-import javafx.application.Platform;
+import com.epam.pricecheckercore.model.inputoutput.CheckerInput;
+import com.epam.pricecheckercore.model.inputoutput.CheckerOutput;
+import com.epam.pricecheckercore.service.checker.Checker;
+import com.epam.pricecheckercore.service.checker.PriceChecker;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.scene.control.ProgressIndicator;
 
-import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author Alexander Diachenko.
  */
 public class MainService extends Service<Void> {
 
-    private String filePath;
-    private Integer urlColumn;
-    private Integer insertColumn;
-    private String savedFilePath;
-    private ProgressIndicator progressIndicator;
+    private final String filePath;
+    private final Integer urlColumn;
+    private final Integer insertColumn;
+    private final String savedFilePath;
+    private final ProgressIndicator progressIndicator;
 
     public MainService(String filePath, Integer urlColumn, Integer insertColumn, String savedFilePath, ProgressIndicator progressIndicator) {
         this.filePath = filePath;
@@ -31,52 +32,23 @@ public class MainService extends Service<Void> {
 
     @Override
     protected Task<Void> createTask() {
-        return new Task<Void>() {
+        return new Task<>() {
             @Override
             protected Void call() throws Exception {
-                Excel excel = new ExcelImpl();
-                List<Magazine> magazines = getMagazines();
-                List<List<Object>> table = excel.read(filePath);
-                for (int index = 0; index < table.size(); index++) {
-                    List<Object> row = table.get(index);
-                    if (row.size() < urlColumn) {
-                        continue;
-                    }
-                    String url = String.valueOf(row.get(urlColumn - 1));
-                    for (Magazine magazine : magazines) {
-                        if (magazine.isThisWebsite(url)) {
-                            String price = magazine.getPrice(magazine.getDocument(url));
-                            insert(row, insertColumn - 1, price);
-                        }
-                    }
-                    int finalIndex = index;
-                    Platform.runLater(() -> progressIndicator.setProgress(0.99 / table.size() * (finalIndex + 1)));
-                }
-                excel.write(table, savedFilePath);
+                Checker checker = new PriceChecker();
+                byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+
+                CheckerInput checkerInput = CheckerInput.builder()
+                        .file(bytes)
+                        .urlIndex(urlColumn - 1)
+                        .insertIndex(insertColumn - 1)
+                        .build();
+
+                CheckerOutput checkerOutput = checker.check(checkerInput);
+
+                Files.write(Paths.get(savedFilePath), checkerOutput.getFile());
                 return null;
             }
         };
-    }
-
-    private void insert(List<Object> row, int column, String price) {
-        while (row.size() <= column) {
-            row.add("");
-        }
-        row.set(column, price);
-    }
-
-    private static List<Magazine> getMagazines() {
-        List<Magazine> magazines = new ArrayList<>();
-        magazines.add(new Makeup());
-        magazines.add(new Korea());
-        magazines.add(new RoseRoseShop());
-        magazines.add(new BeautyNetKorea());
-        magazines.add(new NowZenith());
-        magazines.add(new Rozetka());
-        magazines.add(new KoreaButik());
-        magazines.add(new SweetCorea());
-        magazines.add(new Cosmetea());
-        magazines.add(new Sweetness());
-        return magazines;
     }
 }
